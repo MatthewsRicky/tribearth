082d7c85f763fbeaff9fb1031e998da11d7a66e3
RickyMatthews <matthewsrickypro@gmail.com>
2023-01-26 23:49:38 +0300

    2nd Navbar update

--- tribearth/src/Components/Nav.jsx
+++ tribearth/src/Components/Nav.jsx
@@ -1,28 +1,38 @@
 import React from 'react'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { FaCartPlus } from 'react-icons/fa'
 import Logo from '../assets/Tribearth logo.png'
 
 function Nav() {
   return (
-    <div className='bg-gray-200 w-full fixed top-0 left-0'>
-      <div className="flex flex-col items-center justify-between">
-        <span>
-          <img className='' src={Logo} alt="" />
-        </span>
+    <div className='bg-green-100 w-full fixed top-0 left-0'>
+      <div className="flex flex-row items-center justify-between">
+        <img className='hidden md:flex items-center justify-start pl-4 h-[30px] w-24' src={Logo} alt="/" />
         <ul className='md:flex hidden'>
-          <li className='p-4 text-xl mx-auto text-green-900'><a href="">Story</a></li>
-          <li className='p-4 text-xl mx-auto text-green-900'><a href="">Impact</a></li>
-          <li className='p-4 text-xl mx-auto text-green-900'><a href="">Artisans</a></li>
-          <li className='p-4 text-xl mx-auto text-green-900'><a href="">Vegan cafe</a></li>
-          <li className='p-4 text-xl mx-auto text-green-900'><a href="">Shop</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Story</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Impact</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Artisans</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Vegan cafe</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Shop</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href=""><FaCartPlus /></a></li>
         </ul>
-        <ul>
-          <li>
-            <a href=""></a>
-          </li>
-          <li></li>
-        </ul>
+      </div>
+      <div className='flex'>
+        <div className='flex absolute right-0'>
+          <span className='py-4 px-4 text-xl mx-auto text-black md:hidden'><AiOutlineClose /></span>
+        </div>
+        <img className='md:hidden lg:hidden sm:flex mt-4 items-center justify-start pl-4 h-[30px] w-24' src={Logo} alt="/" />
 
+      </div>
+      <div>
+        <ul className='sm:flex sm:justify-between sm:items-center mx-auto flex-col md:hidden'>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Story</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Impact</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Artisans</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Vegan cafe</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Shop</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black'><a href=""><FaCartPlus /></a></li>
+        </ul>
       </div>
     </div>
   )
