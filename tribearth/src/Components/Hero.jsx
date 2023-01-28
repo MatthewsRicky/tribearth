39a96b5ba656db1d455251529457bb86ab1c9afe
RickyMatthews <matthewsrickypro@gmail.com>
2023-01-28 22:00:05 +0300

    Responsive Navbar, boiler plate hero page

--- tribearth/src/Components/Hero.jsx
+++ tribearth/src/Components/Hero.jsx
@@ -3,8 +3,16 @@ import HeroImage from '../assets/hero.jpg'
 
 function Hero() {
   return (
-    <div className='mt-40 w-full h-full'>
-      <img src={HeroImage} alt="/" />
+    <div>
+      <div
+        className='object-container'
+
+        style={{ display: 'flex', height: '100vh', backgroundImage: `url(${HeroImage})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: '86px' }}>
+
+      </div>
+      <div>
+        <button className="absolute top-[70%] md:top-[50%] left-[46%] flex justify-center items-center bg-slate-200 hover:scale-110 duration-200 text-center rounded-xl px-4 py-2 md:scale-125 md:hover:scale-150"  >EXPLORE</button>
+      </div>
 
     </div>
   )
--- tribearth/src/Components/Nav.jsx
+++ tribearth/src/Components/Nav.jsx
@@ -5,31 +5,35 @@ import { FaCartPlus } from 'react-icons/fa'
 import Logo from '../assets/Tribearth logo.png'
 
 function Nav() {
+  const [navBar, setNavBar] = useState(true)
+
+  const handleNav = () => {
+    setNavBar(!navBar)
+  }
 
   return (
-    <div className='bg-transparent border-b-[2px] shadow-xl w-full fixed top-0 left-0'>
+    <div className='bg-white border-b-[2px] shadow-xl w-full fixed top-0 left-0 mb-40 z-10'>
       <div className="flex flex-row relative my-3 items-center justify-between">
-        <img className='hidden md:flex items-center justify-start pl-16 h-[24px] w-[24]' src={Logo} alt="/" />
+        <img className='hidden md:flex items-center justify-start pl-16 h-[24px] w-[24] hover:scale-[105%] duration-500' src={Logo} alt="/" />
         <ul className='md:flex hidden'>
-          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Story</a></li>
-          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Impact</a></li>
-          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Artisans</a></li>
-          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Vegan cafe</a></li>
-          <li className='py-4 px-4 text-xl mx-auto text-black'><a href="">Shop</a></li>
-          <li className='py-4 px-4 text-xl mx-auto text-black'><a href=""><FaCartPlus /></a></li>
+          <li className='bold py-4 px-4 text-xl mx-auto text-black hover:scale-[105%] hover:highlight duration-200'><a href="">Story</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black hover:scale-[105%] duration-500'><a href="">Impact</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black hover:scale-[105%] duration-500'><a href="">Artisans</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black hover:scale-[105%] duration-500'><a href="">Vegan cafe</a></li>
+        </ul>
+        <ul className='md:flex hidden'>
+          <li className='py-4 px-4 text-xl mx-auto text-black hover:scale-[105%] duration-500'><a href="">Shop</a></li>
+          <li className='py-4 px-4 text-xl mx-auto text-black hover:scale-[105%] duration-500'><a href=""><FaCartPlus /></a></li>
         </ul>
       </div>
       <div className='shadow-xl flex relative justify-between'>
-        <img className='md:hidden lg:hidden mt-4 items-center justify-start pl-16 h-[24px] w-[24]' src={Logo} alt="/" />
-        <div className='hidden absoulte p-4'>
-          <AiOutlineClose size={25} />
-        </div>
-        <div className='md:hidden flex absoulte p-4'>
-          <AiOutlineMenu size={25} />
+        <img className='md:hidden lg:hidden my-4 items-center justify-start pl-16 h-[24px] w-[24] hover:scale-[105%] duration-500' src={Logo} alt="/" />
+        <div className='md:hidden lg:hidden xl:hidden my-4 mr-16' onClick={handleNav}>
+          {!navBar ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
         </div>
       </div>
-      <div className='flex shadow-xl'>
-        <ul className='font-medium text-center sm:items-center mx-auto flex-col w-full hidden boder-b-[2px] duration-500'>
+      <div className={!navBar ? 'flex shadow-xl ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <ul className='md:hidden font-medium text-center sm:items-center mx-auto flex-col w-[90%] bg-white boder-b-[2px] flex duration-500'>
           <li className='py-4 px-4 text-xl mx-auto text-black border-b-[2px] border-gray-300 bg-transparent hover:scale-[105%] duration-500 w-[75%] '><a href="">Story</a></li>
           <li className='py-4 px-4 text-xl mx-auto text-black border-b-[2px] border-gray-300 bg-transparent hover:scale-[105%] duration-500 w-[75%] '><a href="">Impact</a></li>
           <li className='py-4 px-4 text-xl mx-auto text-black border-b-[2px] border-gray-300 bg-transparent hover:scale-[105%] duration-500 w-[75%] '><a href="">Artisans</a></li>
