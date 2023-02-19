456f0269d2d11ff53e4fd99bf13a8db4ed3fdda0
RickyMatthews <matthewsrickypro@gmail.com>
2023-02-19 22:19:59 +0300

    Building out the about page.

--- tribearth/src/Components/Learn.jsx
+++ tribearth/src/Components/Learn.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import Green from '../assets/green.png'
 
 function Learn() {
   return (
-    <div>
-      <div className='w-full h-[50%] bg-emerald-300'>
+    <div style={{
+      backgroundImage: `${<Green />}`,
+      width: '100%',
+      height: '100%',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+    }}>
+      <div
+        className='w-full h-[50%]'>
         <h1 className="text-3xl text-white bold">Learn about our green practices</h1>
         <button className='text-white'>LEARN MORE</button>
       </div>
-    </div>
+    </div >
   )
 }
 
--- tribearth/src/pages/Story.jsx
+++ tribearth/src/pages/Story.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import Nav from '../Components/Nav'
+import Abouthero from '../assets/abouthero.png'
+import Green from '../assets/green.png'
 
 function Storypage() {
   return (
-    <div>
+    <div className='relative '>
       <Nav></Nav>
-      <div>Story Page</div>
+      <div className='flex flex-col justify-around pt-20'>
+        <div className='flex'>
+          <h1 className="text-4xl bold underline underline-offset-8 py-4 mx-auto my-12">About us</h1>
+        </div>
+        <div className='flex flex-col'>
+          <div className='flex mx-8 bg-slate-200'>
+            <p className=" p-2 text-4xl bold ml-8 mr-4px w-[40%]">
+              Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.
+            </p>
+            <div className='p-4'>
+              <img
+                style={
+                  {
+                    width: 'auto',
+                    height: '100%',
+                  }
+                }
+                src={Abouthero} alt="/" />
+            </div>
+          </div>
+
+          <div className="flex">
+            <div className="flex p-4">
+              <img
+                style={{
+                  width: 'auto',
+                  height: '100%'
+                }}
+                src={Green} alt="" />
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
