ddaa33f3e6376a42742a5065604137850d435778
RickyMatthews <matthewsrickypro@gmail.com>
2023-02-20 23:45:42 +0300

    Update Story.jsx

--- tribearth/src/pages/Story.jsx
+++ tribearth/src/pages/Story.jsx
@@ -13,80 +13,92 @@ function Storypage() {
       <div className='flex flex-col justify-around pt-20'>
 
         <div className='flex flex-col p-2'>
-          <div className='flex justify-between items-center h-full flex-col md:flex-row m-8 bg-transparent border-2 shadow-xl'>
+          <div className='border-2 shadow-xl m-10'>
             <div className='flex'>
               <h1 className="text-4xl bold underline underline-offset-8 py-4 mx-auto my-2">About us</h1>
             </div>
-            <p className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
-              Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.
-            </p>
-            <div className='p-4'>
-              <img
-                style={
-                  {
-                    width: 'auto',
-                    height: 'auto',
+            <div className='flex justify-between items-center h-full flex-col md:flex-row bg-transparent'>
+
+              <p className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
+                Tribearth is a socially conscious jewelry business that creates a community of empowered women & youth who produce handmade products in a sustainable and ethical environment. Using a moralistic and economic approach, Tribearth creates a challenging voice against the detrimental impact of fast fashion and inequality faced by stigmatized women & youth.
+              </p>
+              <div className='p-4'>
+                <img
+                  style={
+                    {
+                      width: 'auto',
+                      height: 'auto',
+                    }
                   }
-                }
-                src={Abouthero} alt="/" />
+                  src={Abouthero} alt="/" />
+              </div>
             </div>
           </div>
 
-          <div className='flex justify-between items-center h-full flex-col md:flex-row m-8 bg-transparent border-2 shadow-xl'>
+          <div className='border-2 shadow-xl m-10'>
             <div className='flex'>
               <h1 className="text-4xl bold underline underline-offset-8 py-4 mx-auto my-2">Value & Philosophy</h1>
             </div>
-            <div className="flex p-4">
+            <div className='flex justify-between items-center h-full flex-col md:flex-row bg-transparent'>
+              <div className="flex p-4">
+                <img
+                  style={{
+                    width: 'auto',
+                    height: '100%'
+                  }}
+                  src={Green} alt="" />
+              </div>
+              <div className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
+
+                <ul className='flex flex-col justify-center items-center'>
+                  <li>Sustainabilty</li>
+                  <li>Ethical Trade</li>
+                  <li>Creative Innovation</li>
+                  <li>Passion</li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+
+          <div className='border-2 shadow-xl m-10'>
+            <div className='flex'>
+              <h1 className="text-4xl bold underline underline-offset-8 py-4 mx-auto my-2">Team LocalAid</h1>
+            </div>
+            <div className='flex justify-between items-center h-full flex-col md:flex-row bg-transparent'>
+              <p className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
+                We are a Social Enterprise under our affiliated NGO called LocalAid. LocalAid has 10 years of experience in the NGO sector in terms of measuring impact, social interaction, and outreach programs. LocalAid's vision is to empower marginalized communities and vulnerable children. Tribearth benefits from LocaLAid's organizational architecture, such as legal framework, HR and accounting.
+              </p>
+              <div className='p-4'>
+                <img
+                  style={
+                    {
+                      width: 'auto',
+                      height: '100%',
+                    }
+                  }
+                  src={Localaid} alt="/" />
+              </div>
+            </div>
+          </div>
+
+          <div className='border-2 shadow-xl m-10'>
+            <div className='flex'>
+              <h1 className="text-4xl bold underline underline-offset-8 py-4 mx-auto my-2">Designer</h1>
+            </div>
+            <div className='flex justify-between items-center h-full flex-col md:flex-row bg-transparent'>
               <img
                 style={{
                   width: 'auto',
                   height: '100%'
                 }}
-                src={Green} alt="" />
+                src={Sumer} alt="" />
+              <p className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
+                Sumer Homeh is our head creative designer. She had studied and worked as a fashion designer for 7 years in Australia before founding LocalAid in 2008. She has lead several projects through her humanitarian work in Kenya over the past 10 years with LocalAid.
+              </p>
             </div>
-            <div className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
+          </div>
 
-              <ul className='flex flex-col justify-center items-center'>
-                <li>Sustainabilty</li>
-                <li>Ethical Trade</li>
-                <li>Creative Innovation</li>
-                <li>Passion</li>
-              </ul>
-            </div>
-
-          </div>
-          <div className='flex justify-between items-center h-full flex-col md:flex-row m-8 bg-transparent border-2 shadow-xl'>
-            <div className='flex'>
-              <h1 className="text-4xl bold underline underline-offset-8 py-4 mx-auto my-2">Team LocalAid</h1>
-            </div>
-            <p className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
-              We are a Social Enterprise under our affiliated NGO called LocalAid. LocalAid has 10 years of experience in the NGO sector in terms of measuring impact, social interaction, and outreach programs. LocalAid's vision is to empower marginalized communities and vulnerable children. Tribearth benefits from LocaLAid's organizational architecture, such as legal framework, HR and accounting.
-            </p>
-            <div className='p-4'>
-              <img
-                style={
-                  {
-                    width: 'auto',
-                    height: '100%',
-                  }
-                }
-                src={Localaid} alt="/" />
-            </div>
-          </div>
-          <div className='flex justify-between items-center h-full flex-col md:flex-row m-8 bg-transparent border-2 shadow-xl'>
-            <div className='flex'>
-              <h1 className="text-4xl bold underline underline-offset-8 py-4 mx-auto my-2">Designer</h1>
-            </div>
-            <img
-              style={{
-                width: 'auto',
-                height: '100%'
-              }}
-              src={Sumer} alt="" />
-            <p className=" p-2 text-xl text-left w-full bold ml-8 mx-4 md:w-[40%]">
-              Sumer Homeh is our head creative designer. She had studied and worked as a fashion designer for 7 years in Australia before founding LocalAid in 2008. She has lead several projects through her humanitarian work in Kenya over the past 10 years with LocalAid.
-            </p>
-          </div>
 
         </div>
       </div>
