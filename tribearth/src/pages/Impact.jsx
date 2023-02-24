e5da0650870ca68e7e2a06249f4234368475f16a
RickyMatthews <matthewsrickypro@gmail.com>
2023-02-24 18:01:24 +0300

    Update Impact.jsx

--- tribearth/src/pages/Impact.jsx
+++ tribearth/src/pages/Impact.jsx
@@ -5,53 +5,18 @@ import Ethicalgems from '../assets/Ethicalgems.jpg'
 
 function Impact() {
   return (
-    <div>
-      <Nav></Nav>
-      <div className='flex flex-col justify-center'>
-
-        <div className='relative mt-[8rem] p-4'>
-          <img
-            className='relative left-24 mix-w-sm md:w-[70%] rounded-xl shadow-2xl'
-            src={Ethicalgems} alt="" />
-          <div className='relative top-[-20rem] left-8 rounded-2xl py-6 md:w-[40%] bg-emerald-100 w-[50%]'>
-            <h1 className='text-3xl md:text-4xl text-bol text-center py-4'>Ethical Gemstones</h1>
-            <p className='text-xl px-4 text-center'>Responsible sourcing of gemstones is of great importance to our brand. Our gemstones are sourced directly from ethical mines run by women in Kenya. We embrace traceability and sustainability throughout our complete supply chain.</p>
-          </div>
-        </div>
-
-        <div className='relative mt-[8rem] p-4'>
-          <img
-            className='relative left-24 mix-w-sm md:w-[70%] rounded-xl shadow-2xl'
-            src={Ethicalgems} alt="" />
-          <div className='relative top-[-13rem] left-6 rounded-2xl py-6 md:w-[40%] bg-emerald-100 w-[50%]'>
-            <h1 className='text-3xl md:text-4xl text-bol text-center py-4'>Ethical Gemstones</h1>
-            <p className='text-xl px-4 text-center'>Responsible sourcing of gemstones is of great importance to our brand. Our gemstones are sourced directly from ethical mines run by women in Kenya. We embrace traceability and sustainability throughout our complete supply chain.</p>
-          </div>
-        </div>
-        <div className='relative mt-[8rem] p-4'>
-          <img
-            className='relative left-24 mix-w-sm md:w-[70%] rounded-xl shadow-2xl'
-            src={Ethicalgems} alt="" />
-          <div className='relative top-[-20rem] left-8 rounded-2xl py-6 md:w-[40%] bg-emerald-100 w-[50%]'>
-            <h1 className='text-3xl md:text-4xl text-bol text-center py-4'>Ethical Gemstones</h1>
-            <p className='text-xl px-4 text-center'>Responsible sourcing of gemstones is of great importance to our brand. Our gemstones are sourced directly from ethical mines run by women in Kenya. We embrace traceability and sustainability throughout our complete supply chain.</p>
-          </div>
-        </div>
-
-        <div className='relative mt-[8rem] p-4'>
-          <img
-            className='relative left-24 mix-w-sm md:w-[70%] rounded-xl shadow-2xl'
-            src={Ethicalgems} alt="" />
-          <div className='relative top-[-13rem] left-6 rounded-2xl py-6 md:w-[40%] bg-emerald-100 w-[50%]'>
-            <h1 className='text-3xl md:text-4xl text-bol text-center py-4'>Ethical Gemstones</h1>
-            <p className='text-xl px-4 text-center'>Responsible sourcing of gemstones is of great importance to our brand. Our gemstones are sourced directly from ethical mines run by women in Kenya. We embrace traceability and sustainability throughout our complete supply chain.</p>
-          </div>
-        </div>
+    <div className='relative'>
+      <Nav></Nav >
 
 
+
+      <div className='flex flex-col my-24 rounded-2xl py-6 md:w-[40%] bg-emerald-100 w-[50%]'>
+        <h1 className='text-3xl md:text-4xl text-bol text-center py-4'>Ethical Gemstones</h1>
+        <p className='text-xl px-4 text-center'>Responsible sourcing of gemstones is of great importance to our brand. Our gemstones are sourced directly from ethical mines run by women in Kenya. We embrace traceability and sustainability throughout our complete supply chain.</p>
       </div>
+
       <Footer />
-    </div>
+    </div >
   )
 }
 
