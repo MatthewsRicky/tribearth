66b602006405045ed8f302fba26bd0b111f86d42
RickyMatthews <matthewsrickypro@gmail.com>
2023-03-06 21:18:54 +0300

    Update Carousel.jsx

--- tribearth/src/Components/Carousel.jsx
+++ tribearth/src/Components/Carousel.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 function Carousel() {
   return (
