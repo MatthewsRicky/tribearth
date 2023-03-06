6398d6a655d993fe5aba94d396fda0110b9bcc83
RickyMatthews <matthewsrickypro@gmail.com>
2023-03-06 21:22:41 +0300

    Update Carousel.jsx

--- tribearth/src/Components/Carousel.jsx
+++ tribearth/src/Components/Carousel.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Athens from '../assets/RingsSliderImages/Athens Delight.jpeg'
+import Aurora from '../assets/RingsSliderImages/Auroras Light.jpeg'
+import Bacchus from '../assets/RingsSliderImages/bands of bacchus.jpeg'
+import Delilah from '../assets/RingsSliderImages/Bands of Delilah.jpeg'
+import Lilac from '../assets/RingsSliderImages/Drops of Lilac.jpeg'
+import Osiris from '../assets/RingsSliderImages/Eye of Osiris.jpeg'
+import Golden from '../assets/RingsSliderImages/Golden Spiral.jpeg'
+import Kali from '../assets/RingsSliderImages/Kalis Crown.jpeg'
+import Ocean from '../assets/RingsSliderImages/Ocean Peace.jpeg'
+import Purple from '../assets/RingsSliderImages/Purple Moon.jpeg'
+import Sirene from '../assets/RingsSliderImages/Sirenes Wings.jpeg'
+import Talisman from '../assets/RingsSliderImages/Talismans Tale.jpeg'
 
 
 function Carousel() {
+
+  const images = [
+    Athens,
+    Aurora,
+    Bacchus,
+    Delilah,
+    Lilac,
+    Osiris,
+    Golden,
+    Kali,
+    Ocean,
+    Purple,
+    Sirene,
+    Talisman,
+  ];
+
+
   return (
     <div>Carousel</div>
   )
